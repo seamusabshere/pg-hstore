6af486a0abc146776e291eef7272faa2782429e8
Greg Price <price@mit.edu>
2013-02-19 20:15:08 -0800

    More documentation of behavior and upstream spec
    Also refactor the private method '.escape' so that we
    can better describe its function in terms of the spec.
    This commit should have no effect on behavior.
    
    We shorten the non-comment code by 10 lines in net,
    while adding 26 lines of comments.

--- lib/pg_hstore.rb
+++ lib/pg_hstore.rb
@@ -21,22 +21,28 @@ module PgHstore
     end
   end
 
-  # set for_parameter = true if you're using the output for a bind variable
-  def PgHstore.dump(hash, for_parameter = false)
-    memo = hash.map do |k, v|
-      if v.nil?
-        v = "NULL"
-      else
-        v = DOUBLE_QUOTE + escape(v) + DOUBLE_QUOTE
-      end
-      k = DOUBLE_QUOTE + escape(k) + DOUBLE_QUOTE
-      [k, v].join HASHROCKET
-    end.join COMMA
-    if for_parameter
-      memo
-    else
-      as_postgresql_string_constant(memo)
-    end
+  # Serialize a hash to be sent to PostgreSQL as an hstore value.
+  #
+  # By default, returns a (Postgre)SQL string constant suitable for
+  # interpolating directly into a query.  With raw_string = true,
+  # returns the plain string value suitable for use as a bind variable.
+  def PgHstore.dump(hash, raw_string = false)
+    # Per http://www.postgresql.org/docs/9.2/static/hstore.html :
+    #
+    #   The text representation of an hstore, used for input and
+    #   output, includes zero or more 'key => value' pairs separated
+    #   by commas. [...] Whitespace between pairs or around the =>
+    #   sign is ignored.  Double-quote keys and values [... see
+    #   escape_nonnull_for_hstore ...]
+    #
+    #   A value (but not a key) can be an SQL NULL.  Double-quote the
+    #   NULL to treat it as the ordinary string "NULL".
+    hstore = hash.map do |k, v|
+      hstore_k = escape_nonnull_for_hstore(k)
+      hstore_v = (v.nil?) ? "NULL" : escape_nonnull_for_hstore(v)
+      [hstore_k, hstore_v].join(HASHROCKET)
+    end.join(COMMA)
+    raw_string ? hstore : as_postgresql_string_constant(hstore)
   end
 
   class << self
@@ -59,14 +65,24 @@ module PgHstore
   def PgHstore.unescape(literal)
     literal.gsub ESCAPED_CHAR, '\1'
   end
-  
-  NON_ESCAPE_SLASH = '\\'
-  ESCAPED_SLASH = '\\\\'
-  ESCAPED_DOUBLE_QUOTE = '\"'
-  def PgHstore.escape(string)
-    string.to_s.gsub(NON_ESCAPE_SLASH) {ESCAPED_SLASH}.gsub DOUBLE_QUOTE, ESCAPED_DOUBLE_QUOTE
+
+  # Escape a value as a string to use as an hstore key or value.
+  #
+  # Per http://www.postgresql.org/docs/9.2/static/hstore.html :
+  #
+  #   Double-quote keys and values that include [stuff]. To include a
+  #   double quote or a backslash in a key or value, escape it with a
+  #   backslash.
+  #
+  # You got it, boss.
+  def PgHstore.escape_nonnull_for_hstore(string)
+    interior = string.to_s.gsub('\\') {'\\\\'}.gsub('"', '\\"')
+    '"' + interior + '"'
   end
 
+  # Escape a string as a string constant to be used in a SQL query
+  # to PostgreSQL.
+  #
   # Ideally we would use plain SQL string constants, which are very simple:
   #   http://www.postgresql.org/docs/9.2/static/sql-syntax-lexical.html#SQL-SYNTAX-STRINGS
   # Unfortunately PostgreSQL treats these differently depending on the
